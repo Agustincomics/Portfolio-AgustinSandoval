3a948a4876f843394d3403f98a7c6dd5d5a5ac03
Agustin Sandoval <94272906+Agustincomics@users.noreply.github.com>
2023-08-02 19:43:53 -0300

    Se desarrollo  el maquetado de Experiencia.jsx

---
+++ src/components/views/Experiencia.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Experiencia = () => {
+    return (
+        <Container className='text-light p-4' id='experiencia'>
+            <h3 className='text-center titulo mb-3'>Trabajo</h3>
+            <h6>Me encuentro en búsqueda de formar parte de una empresa del sector IT, mientras tanto continuo capacitándome y desarrollando nuevas habilidades.
+                Actualmente estoy cursando la carrera de Tecnincatura Superior en Programacion, estoy amplando mis conocimientos y creando nuevos proyectos.
+            </h6>
+        </Container>
+    );
+};
+
+export default Experiencia;
